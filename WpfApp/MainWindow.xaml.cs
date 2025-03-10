615125b378d5f300f7bfafeaeb6653f435c48d78
vanso <vanso@VANSON>
2025-03-10 17:39:15 +0700

    Add project files.

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private string _message;
+    public string Message
+    {
+        get { return _message; }
+        set
+        {
+            _message = value;
+            OnPropertyChanged(nameof(Message));
+        }
+    }
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        Message = "Nguyen Van Son";
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void ChangeMessage(object sender, RoutedEventArgs e)
+    {
+        Message = "Updated Text";
+    }
+}
